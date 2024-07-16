9ab4aad6c6c745c97ed9e9cb7cb377ed06fc30ab
Eleven <idiotalex@163.com>
2024-07-17 00:01:21 +0800

    chore: add unplugin unocss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,45 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          enabledCollections: [
+            'mdi',
+          ],
+        }),
+      ],
+      dts: './components.d.ts',
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+      ],
+      dts: './auto-imports.d.ts',
+    }),
+    Icons({
+      autoInstall: true,
+      compiler: 'vue3',
+    }),
   ],
   resolve: {
     alias: {
