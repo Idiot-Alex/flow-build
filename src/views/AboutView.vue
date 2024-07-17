0d6c7cd31bce80d24ae8eb89a9a8d53724db5a81
Eleven <idiotalex@163.com>
2024-07-17 17:58:42 +0800

    chore: add vue-flow tools

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,20 @@
 <script lang="ts" setup>
-import { VueFlow } from '@vue-flow/core'
+import { VueFlow, Handle, Position } from '@vue-flow/core'
+import { Background } from '@vue-flow/background'
+import { MiniMap } from '@vue-flow/minimap'
+import { Controls } from '@vue-flow/controls'
+import { NodeResizer } from '@vue-flow/node-resizer'
+import { NodeToolbar } from '@vue-flow/node-toolbar'
 
 const nodes = ref([
   { 
     id: '1', 
     position: { x: 250, y: 5 },
     data: { label: 'Node 1' },
-    
     // Add a class name to the node
-    class: 'my-custom-node-class',
-    
+    class: 'vue-flow__node-custom',
     // You can pass an object containing CSSProperties or CSS variables
-    style: { backgroundColor: 'green', width: '200px', height: '100px' },
+    style: { },
   },
 ])
 </script>
@@ -19,11 +22,24 @@ const nodes = ref([
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <NodeToolbar>
+      <button>delete</button>
+      <button>copy</button>
+      <button>expand</button>
+    </NodeToolbar>
+
+    
+    <!-- <Handle type="target" :position="Position.Left" />
+    <Handle type="source" :position="Position.Right" /> -->
     <VueFlow
       :nodes="nodes"
-      class="my-diagram-class"  
-      :style="{ background: 'red' }"
-    />
+      class="my-diagram-class"
+    >
+      <Background />
+      <MiniMap />
+      <Controls />
+      <NodeResizer min-width="100" min-height="30" />
+    </VueFlow>
   </div>
 </template>
 
@@ -32,20 +48,20 @@ const nodes = ref([
 @import '@vue-flow/core/dist/style.css';
 /* this contains the default theme, these are optional styles */
 @import '@vue-flow/core/dist/theme-default.css';
+/* import default minimap styles */
+@import '@vue-flow/minimap/dist/style.css';
+/* import default controls styles */
+@import '@vue-flow/controls/dist/style.css';
+/* make sure to include the necessary styles! */
+@import '@vue-flow/node-resizer/dist/style.css';
+
 /* Use a purple theme for our custom node */
 .vue-flow__node-custom {
-    background: purple;
-    color: white;
-    border: 1px solid purple;
-    border-radius: 4px;
-    box-shadow: 0 0 0 1px purple;
-    padding: 8px;
-}
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+  background: purple;
+  color: white;
+  border: 1px solid purple;
+  border-radius: 4px;
+  box-shadow: 0 0 0 1px purple;
+  padding: 8px;
 }
 </style>
