04c9ceb836a44e2d02f7d569fb455c50d77e49f6
Eleven <idiotalex@163.com>
2024-08-27 19:41:07 +0800

    chore: add Breadcrumbs

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="breadcrumbs text-sm pl-4">
+    <ul>
+      <li><a>Home</a></li>
+      <li><a>Documents</a></li>
+      <li>Add Document</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
+import BreadCrumbs from '../components/BreadCrumbs.vue'
 </script>
 
 <template>
   <main>
+    <Header />
+    <BreadCrumbs />
     <TheWelcome />
   </main>
 </template>
