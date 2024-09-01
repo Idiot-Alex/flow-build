baea01886a56aa877ec2bf9bc783fc746c118cfb
Eleven <idiotalex@163.com>
2024-09-01 14:21:24 +0800

    chore: fix pagination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,8 @@ const toast = useToast()
 const flowList = ref<Res[]>([])
 const pagination = ref({
   page: 1,
-  size: 10,
+  size: 5,
+  pages: 0,
   total: 0,
 })
 
@@ -21,12 +22,28 @@ const loadFlowList = async () => {
     flowList.value = res.data
     pagination.value.page = res.page
     pagination.value.size = res.size
+    pagination.value.pages = res.pages
     pagination.value.total = res.total
 
     toast.showSuccess(res.msg)
   } else {
     toast.showError(res.msg)
   }
+}
+
+const prevPage = () => {
+  pagination.value.page --
+  loadFlowList()
+}
+
+const nextPage = () => {
+  pagination.value.page ++
+  loadFlowList()
+}
+
+const changePageSize = () => {
+  pagination.value.page = 1
+  loadFlowList()
 }
 
 onMounted(async () => {
@@ -61,13 +78,14 @@ onMounted(async () => {
     </div>
     <div class="flex justify-center gap-4">
       <div class="join grid grid-cols-2">
-        <button class="join-item btn btn-outline">上一页</button>
-        <button class="join-item btn btn-outline">下一页</button>
+        <button class="join-item btn btn-outline" :disabled="pagination.page <= 1" @click="prevPage">上一页</button>
+        <button class="join-item btn btn-outline" :disabled="pagination.page >= pagination.pages" @click="nextPage">下一页</button>
       </div>
       <div class="join grid grid-cols-2 items-center gap-2">
-        <span class="join-item">第 {{ pagination.page }} 页 / 共 {{ pagination.total }} 条</span>
-        <select class="select select-bordered" v-model="pagination.size">
-          <option value="10" selected>10 条</option>
+        <span class="join-item">第 {{ pagination.page }} / {{ pagination.pages }} 页 / 共 {{ pagination.total }} 条</span>
+        <select class="select select-bordered" v-model="pagination.size" @change="changePageSize">
+          <option value="5" selected>5 条</option>
+          <option value="10">10 条</option>
           <option value="50" >50 条</option>
           <option value="100" >100 条</option>
         </select>
