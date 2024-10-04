ab121a3a6b740dc61fb28fbb4284abb07116efc6
Eleven <idiotalex@163.com>
2024-10-04 23:55:33 +0800

    feat: fix code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,6 +14,11 @@ const toast = useToast()
   <main class="main-wrapper">
     <RouterView />
   </main>
+  <footer class="footer footer-center bg-base-300 text-base-content p-4">
+    <aside>
+      <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    </aside>
+  </footer>
 </template>
 
 <style scoped>
